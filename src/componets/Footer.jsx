21da3dc17a44520f09913d9d78e26de7ca72a65b
abul kalam <bd.abulkalam800@gamil.com>
2025-06-14 10:46:52 +0300

    added footer social logo

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -1,49 +1,48 @@
 import React from "react";
+import logo from "../assets/logohomeservices1.png";
+
+import { FaFacebookF, FaLinkedinIn, FaYoutube } from "react-icons/fa";
+import { Link } from "react-router";
 
 const Footer = () => {
   return (
+    <div>
     <footer className="footer  bg-base-200 text-base-content p-10">
-    <div className="max-w-7xl mx-auto  grid grid-cols-1 md:grid-cols-4 md:gap-x-12 gap-5  justify-between">
-        <aside>
-        <svg
-          width="50"
-          height="50"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="fill-current"
-        >
-          <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-        </svg>
-        <p>
-          ACME Industries Ltd.
-          <br />
-          Providing reliable tech since 1992
-        </p>
-      </aside>
-      <nav className="flex flex-col">
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav className="flex flex-col">
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav className="flex flex-col" >
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
-    </div>
+      <div className="max-w-7xl mx-auto  grid grid-cols-1 md:grid-cols-4 md:gap-x-12 gap-5  items-center justify-between ">
+       <Link to='/'>
+          <img src={logo} alt="logo"  className="w-[300px]"/>
+      </Link>
+        <nav className="flex flex-col gap-3 ">
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Ac-Repair</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Elctrical</a>
+          <a className="link link-hover">Plumbing</a>
+        </nav>
+
+        <nav className="flex flex-col gap-3">
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+        <div>
+          <h3 className="footer-title">Social Links</h3>
+          <ul className="flex justify-center items-center gap-5 mt-10">
+
+            <li><Link to="https://www.facebook.com/bd.abulkalam800" className="text-blue-500"><FaFacebookF size={30}/></Link></li>
+            <li><Link to='https://www.linkedin.com/in/mohammad-abul-kalam-226779303/' className="text-blue-500"><FaLinkedinIn  size={30}/> </Link></li>
+            <li><Link to='https://www.youtube.com/@AbulKalam-800' className="text-blue-500"><FaYoutube size={30} /></Link></li>
+          </ul>
+          
+        </div>
+      </div>
     </footer>
+
+    <div className="bg-base-300 py-10">
+      <p className=" text-center "><span>Copyright: All rights reserved by Ak.services 2025 </span></p>
+    </div>
+    </div>
   );
 };
 
